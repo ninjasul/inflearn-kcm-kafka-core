fcb6583ed7a6589a914bdde4280d9e93e70854d6
dongyub.park <dongyub.park@kurlycorp.com>
2023-01-26 09:12:13 +0900

    Initial commit

---
+++ producers/src/main/java/kafka/SimpleProducer.java
@@ -0,0 +1,26 @@
+package kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "34.64.226.236:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+        String topicName = "simple-topic";
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "hello world");
+
+        kafkaProducer.send(producerRecord);
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
+}
