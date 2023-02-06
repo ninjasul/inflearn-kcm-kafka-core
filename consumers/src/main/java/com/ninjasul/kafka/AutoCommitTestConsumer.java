423888268e254519750408a24314303a09fb9b9e
dongyub.park <dongyub.park@kurlycorp.com>
2023-02-07 08:14:22 +0900

    AutoCommitTestConsumer, AutoCommitWihoutWakeUpTestConsumer, AutoCommitTestProducer 구현

---
+++ consumers/src/main/java/com/ninjasul/kafka/AutoCommitTestConsumer.java
@@ -0,0 +1,96 @@
+package com.ninjasul.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Properties;
+
+public class AutoCommitTestConsumer {
+    public static final Logger log = LoggerFactory.getLogger(AutoCommitTestConsumer.class);
+
+    public static void main(String[] args) {
+        Properties props = buildProperties();
+
+        String topicName = "auto-commit-topic";
+        KafkaConsumer<String, String> kafkaConsumer = buildConsumer(props, topicName);
+
+        // add a ShutdownHook for the thread to invoke wakeup() method and wait until the main thread is terminated.
+        addShutdownHookToCallWakeup(kafkaConsumer);
+
+        consume(kafkaConsumer);
+    }
+
+    private static Properties buildProperties() {
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "34.64.226.236:9092");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "auto-commit-group-01");
+        props.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "6000");
+        return props;
+    }
+
+    private static KafkaConsumer<String, String> buildConsumer(Properties props, String topicName) {
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(props);
+        kafkaConsumer.subscribe(List.of(topicName));
+        return kafkaConsumer;
+    }
+
+    private static void addShutdownHookToCallWakeup(KafkaConsumer<String, String> kafkaConsumer) {
+        Thread mainThread = Thread.currentThread();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("The wakeup thread starts to terminate consumer by calling wakeup() method.");
+            kafkaConsumer.wakeup();
+
+            try {
+                mainThread.join();
+            } catch (InterruptedException e) {
+                log.error("", e);
+            }
+        }));
+    }
+
+    private static void consume(KafkaConsumer<String, String> kafkaConsumer) {
+        pollAutoCommit(kafkaConsumer);
+    }
+
+    private static void pollAutoCommit(KafkaConsumer<String, String> kafkaConsumer) {
+        int loopCnt = 1;
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofMillis(1000L));
+                log.info("####### loopCnt: {}, consumerRecords count: {} ######", loopCnt++, records.count());
+                for (ConsumerRecord record : records) {
+                    log.info("record key: {}, record value: {}, partition: {}, record offset: {}",
+                            record.key(),
+                            record.value(),
+                            record.partition(),
+                            record.offset()
+                    );
+                }
+
+                try {
+                    log.info("The main thread is sleeping for {} ms during while loop.", 10000);
+                    Thread.sleep(10000L);
+                } catch (InterruptedException e) {
+                    log.error("", e);
+                }
+            }
+        } catch (WakeupException e) {
+            log.warn("A WakeupException has been thrown.", e);
+        } finally {
+            log.info("The kafka consumer is closing.");
+            kafkaConsumer.close();
+        }
+    }
+}
---
+++ producers/src/main/java/kafka/AutoCommitTestProducer.java
@@ -0,0 +1,98 @@
+package kafka;
+
+import com.github.javafaker.Faker;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.Random;
+
+public class AutoCommitTestProducer {
+    public static final Logger log = LoggerFactory.getLogger(AutoCommitTestProducer.class);
+
+    public static void sendPizzaMessage(KafkaProducer<String, String> kafkaProducer,
+                                        String topicName, int iterCount,
+                                        int interIntervalMillis, int intervalMillis,
+                                        int intervalCount, boolean sync) {
+
+        PizzaMessage pizzaMessage = new PizzaMessage();
+        int iterSeq = 0;
+        long seed = 2022;
+        Random random = new Random(seed);
+        Faker faker = Faker.instance(random);
+
+        long startTime = System.currentTimeMillis();
+
+        while( iterSeq++ != iterCount ) {
+            HashMap<String, String> pMessage = pizzaMessage.produce(faker, random, iterSeq);
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName,
+                    pMessage.get("key"), pMessage.get("message"));
+            sendMessage(kafkaProducer, producerRecord, pMessage, sync);
+
+            if((intervalCount > 0) && (iterSeq % intervalCount == 0)) {
+                try {
+                    log.info("####### IntervalCount: {}, intervalMillis: {}  #########", intervalCount, intervalMillis);
+                    Thread.sleep(intervalMillis);
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage());
+                }
+            }
+
+            if(interIntervalMillis > 0) {
+                try {
+                    log.info("interIntervalMillis:" + interIntervalMillis);
+                    Thread.sleep(interIntervalMillis);
+                } catch (InterruptedException e) {
+                    log.error(e.getMessage());
+                }
+            }
+
+        }
+        long endTime = System.currentTimeMillis();
+        long timeElapsed = endTime - startTime;
+
+        log.info("{} millisecond elapsed for {} iterations", timeElapsed, iterCount);
+    }
+
+    public static void sendMessage(KafkaProducer<String, String> kafkaProducer,
+                                   ProducerRecord<String, String> producerRecord,
+                                   HashMap<String, String> pMessage, boolean sync) {
+        if(!sync) {
+            kafkaProducer.send(producerRecord, (metadata, exception) -> {
+                if (exception == null) {
+                    log.info("async message: {}, partition: {}, offset: {}", pMessage.get("key"), metadata.partition(), metadata.offset());
+                } else {
+                    log.error("exception error from broker {}", exception.getMessage());
+                }
+            });
+        } else {
+            try {
+                RecordMetadata metadata = kafkaProducer.send(producerRecord).get();
+                log.info("sync message: {}, partition: {}, offset: {}", pMessage.get("key"), metadata.partition(), metadata.offset());
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Properties props  = new Properties();
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "34.64.226.236:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        String topicName = "auto-commit-topic";
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+        sendPizzaMessage(kafkaProducer, topicName,
+                -1, 1000, 0, 0, false);
+
+        kafkaProducer.close();
+    }
+}
